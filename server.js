f80822a6af4914a9efb547e934b3b4af80cad79f
Brandon Perez <brandonp940@gmail.com>
2020-08-01 15:06:37 -0400

    fixed formating

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// handels data paring
+// handels data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./public/routes/htmlRoutes")(app);
 require("./public/routes/apiRoutes")(app);
-require("./db/db.json")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
